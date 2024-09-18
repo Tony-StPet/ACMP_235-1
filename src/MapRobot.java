9ee761ce9ea622432f8dfa1c43bcfe027a310378
Tony-StPet <135145428+Tony-StPet@users.noreply.github.com>
2024-09-19 00:40:09 +0300

    Update MapRobot.java

--- src/MapRobot.java
+++ src/MapRobot.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class MapRobot {
+import java.util.Scanner;
+public class MapRobot {
+
+    // создаем матрицу и заполняем нулями - это поле
+    public static int[][] createField() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Запрашиваем у пользователя количество строк и столбцов
+        System.out.print("Введите количество строк: ");
+        int rows = scanner.nextInt();
+        System.out.print("Введите количество столбцов: ");
+        int columns = scanner.nextInt();
+
+        // Создаем матрицу
+        int[][] field = new int[rows][columns];
+
+// Заполняем матрицу нулями
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                field[i][j] = 0;
+            }
+        }
+
+        return field;
+    }
+
+    // Функция для вывода матрицы
+    public static void printField(int[][] field) {
+        for (int[] row : field) {
+            for (int value : row) {
+                System.out.printf("%4d ", value);
+            }
+            System.out.println();
+        }
+    }
+
+
 }
